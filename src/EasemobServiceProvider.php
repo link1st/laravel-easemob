8d07ebc9c39acedc3eaef2540e6de31992c9de60
link <link@hongjitech.cn>
2016-12-06 14:25:07 +0800

    update

--- src/EasemobServiceProvider.php
+++ src/EasemobServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class TestServiceProvider extends ServiceProvider {
+class EasemobServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
